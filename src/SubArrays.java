562e449fba033b644063aaa18b6d41a4c04fd87e
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2024-01-25 23:18:36 +0530

    find-all-subarrays

---
+++ src/SubArrays.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SubArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]= sc.nextInt();
+        }
+
+        subArray(arr,size);
+
+    }
+
+    static void subArray(int[] arr,int size){
+        for(int i=0;i<size;i++){
+            for(int j=i;j<size;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
