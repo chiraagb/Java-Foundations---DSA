b7406c8c091807cc0d9d1e5083144ccbc4d1942b
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2024-02-18 13:48:02 +0530

    updated-rotate-by-90

--- src/RotateBy90.java
+++ src/RotateBy90.java
@@ -4,9 +4,10 @@ public class RotateBy90 {
 
         transpose(arr);
         reverse(arr);
-        for (int[] ints : arr) {
-            for (int j = 0; j < arr[0].length; j++) {
-                System.out.print(ints[j] + " ");
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
