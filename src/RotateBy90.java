087365c6d97990f6d101ce9210ef08cc310e8b0a
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2024-02-18 13:42:47 +0530

    rotate-matrix-by-90

---
+++ src/RotateBy90.java
@@ -0,0 +1,37 @@
+public class RotateBy90 {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+
+        transpose(arr);
+        reverse(arr);
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void transpose(int[][] matrix){
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0; j<i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    static void reverse(int[][] matrix){
+        for(int i=0;i< matrix.length;i++){
+            int start = 0;
+            int end = matrix[0].length - 1;
+            while (start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
